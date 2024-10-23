b2fff0f8b14c577ec213e3321629f9586dad4dc4
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-23 11:10:47 +0800

    Create arithmetic.py

---
+++ modul python/arithmetic.py
@@ -0,0 +1,20 @@
+def penjumlahan(x, y):
+    return x + y
+
+def pengurangan(x, y):
+    return x - y
+
+def perkalian(x, y):
+    return x * y
+
+def pembagian(x, y):
+    return x // y
+
+def perpangkatan(x, y):
+    return x ** y
+
+def bagihabis(x, y):
+    return x // y
+
+def modulus(x, y):
+    return x % y
